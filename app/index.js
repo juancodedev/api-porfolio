3790e0cbbc920a93dbd0bbb380289823b7885f4c
Juan Muñoz <cl.jmunoz@gmail.com>
2024-09-10 01:56:43 -0300

    Se agrega metodo add

--- app/index.js
+++ app/index.js
@@ -26,6 +26,47 @@ app.get('/sql', async (request, response)=>{
   response.send(JSON.stringify(resultadoSQL.rows))
 
 })
+
+app.post('/sql/agregar', async (req, res)=>{
+  const result = {
+    status: '',
+    message: '',
+    data : '',
+  }
+  const {first_name,last_name, email_address,phone_number, services, message} = req.body;
+  // console.log('body: ', req.body)
+
+  const dateNow = new Date();
+  try {
+    const resultadoSQL = await pool.query(
+      'INSERT INTO contacts (first_name,last_name, email_address,phone_number, services, message, created_at, update_at) VALUES ($1, $2, $3, $4, $5, $6, $7,$8) RETURNING *', 
+      [first_name,last_name, email_address,phone_number, services, message, dateNow, dateNow])
+    // console.log(JSON.stringify(resultadoSQL.rows))
+    res.status(201)
+      
+    result.data = resultadoSQL.rows[0]
+    result.status = res.statusCode
+    result.message = 'Registro agregado correctamente'
+    console.log(result)
+
+
+    res.send(JSON.stringify(result))
+    res.end()
+
+  } catch (error) {
+    res.status(500)
+    result.data = req.body
+    result.status = res.statusCode
+    result.message = 'Error al agregar el registro, registro ya existe'
+    res.send(JSON.stringify(result))
+    res.end()
+  }
+
+})
+
+
+
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}/`)

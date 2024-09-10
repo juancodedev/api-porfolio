ee50f684c7f7ac6d45d70fc11fd817e0d4e35520
Juan Muñoz <cl.jmunoz@gmail.com>
2024-09-10 01:53:54 -0300

    Nuevos ajustes

--- app/index.js
+++ app/index.js
@@ -1,18 +1,31 @@
-const express = require('express')
-const { Pool } = require('pg')
-const https = require('https');
+const express = require("express");
+const { Pool } = require("pg");
+const https = require("https");
 
-const app = express()
-const cors = require('cors');
-app.use(cors())
-require('dotenv').config()
+const app = express();
+const cors = require("cors");
+app.use(cors());
+require("dotenv").config();
 
+const pool = new Pool({
+  connectionString: process.env.STRING_DB,
+});
 
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.send('API REST')
-  })
+app.get("/", (req, res) => {
+  res.send("API REST");
+});
 
+app.get('/sql', async (request, response)=>{
+
+  // const resultadoSQL = await sql<Contact>`SELECT * FROM contacts`;
+  const resultadoSQL = await pool.query('SELECT * FROM contacts')
+  console.log(resultadoSQL.rows)
+
+  response.send(JSON.stringify(resultadoSQL.rows))
+
+})
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}/`)

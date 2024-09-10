5eacdd4fc3ebcb45aaec3759bc7058a1056c2749
Juan Muñoz <cl.jmunoz@gmail.com>
2024-09-10 01:53:36 -0300

    Se incorpora consulta SQL

--- app/index.js
+++ app/index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const { Pool } = require('pg')
 const https = require('https');
-const app = express();
 
-const port = process.env.PORT || 3001;
+const app = express()
+const cors = require('cors');
+app.use(cors())
+require('dotenv').config()
+
+
 
 app.get('/', (req, res)=>{
     res.send('API REST')
   })
 
-app.listen(port, () => {
-  console.log(`port runing in http://localhost:${port}`);
-});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}/`)
+);
